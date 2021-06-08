c5503f34c495b8e92b4471343cfe278503d08ee3
Navilongo <navilongo@gmail.com>
2021-06-07 21:45:41 -0700

    test different zooms and coordinates

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -16,6 +16,33 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 var earthquake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
+function makeRadius(magnitude) {
+  if (magnitude < 4.9) {
+    var rad = 2;
+    return rad;
+  }
+  else if (magnitude > 5.0 && magnitude <= 5.4) {
+    var rad = 3;
+    return rad;
+  }
+  else if (magnitude > 5.4 && magnitude <= 5.9) {
+    var rad = 4;
+    return rad;
+  }
+  else if (magnitude > 5.9 && magnitude <= 6.4) {
+    var rad = 5;
+    return rad;
+  }
+  else if (magnitude > 6.4 && magnitude <= 6.9) {
+    var rad = 6;
+    return rad;
+  }
+  else {
+    var rad = 7;
+    return rad;
+  }
+};
+
 d3.json(earthquake_url).then(function(data) {
   geojson = L.geoJson(data, {
     style: function(feature) {
