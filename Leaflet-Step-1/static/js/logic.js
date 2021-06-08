a3070705a0a81e9d6f6e9270f511970f5b735e49
Navilongo <navilongo@gmail.com>
2021-06-07 22:20:18 -0700

    boldened marker pop up

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -82,7 +82,7 @@ d3.json(earthquake_url).then(function(data) {
       };
     },
     onEachFeature: function(feature, layer) {
-      layer.bindPopup("Magnitude:<br>" + feature.properties.mag + "<br>Place:<br>" + feature.properties.place)
+      layer.bindPopup("<strong>Magnitude:<br></strong>" + feature.properties.mag + "<strong><br>Place:<br></strong>" + feature.properties.place)
     },
     pointToLayer: function(geoJsonPoint, latlng) {
       return L.circleMarker(latlng);
