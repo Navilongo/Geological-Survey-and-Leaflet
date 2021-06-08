f9f3ca9809ad9d2027b3afa02f409fc603a29492
Navilongo <navilongo@gmail.com>
2021-06-07 21:21:56 -0700

    pop ups

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -2,38 +2,45 @@
 var myMap = L.map("map", {
     center: [34.0522, -118.2437],
     zoom: 5
-  });
+});
   
-  // Adding tile layer
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
 
-  var earthquake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var earthquake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-  d3.json(earthquake_url, function(data) {
-    var earthquakes = L.geoJSON(data.features, {
-      onEachFeature: addPopup,
-      pointToLayer: addMarker
-    });
-
-    function addMarker(feature, location) {
-      var options = {
-        stroke: false,
-        fillOpacity: markerOpacity(feature.properties.mag),
-        color: markerColor(feature.properties.mag),
-        fillColor: markerColor (feature.properties.mag),
-        radius: markerSize(feature.properties.mag)
-      }
-
-      return L.circleMarker(location, options);
-    }
-
-
+d3.json(earthquake_url, function(data) {
+  var earthquakes = L.geoJSON(data.features, {
+    onEachFeature: addPopup,
+    pointToLayer: addMarker
   });
+
+  createMap(earthquakes) 
+})
+
+function addMarker(feature, location) {
+  var options = {
+    stroke: false,
+    fillOpacity: markerOpacity(feature.properties.mag),
+    color: markerColor(feature.properties.mag),
+    fillColor: markerColor (feature.properties.mag),
+    radius: markerSize(feature.properties.mag)
+  }
+
+  return L.circleMarker(location, options);
+
+}
+
+function addPopup(feature, layer) {}
+
+
+
+
 
